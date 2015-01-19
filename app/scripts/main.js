3274184293ceef762190b847d5e26530f7968468
Patrick Love <plove13x@gmail.com>
2015-01-19 03:17:01 -0500

    Achieves primary functionality, commits while waiting for Heroku to fix servers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+
+var serverUrl = "http://tiny-pizza-server.herokuapp.com/collections/plove";
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', serverUrl, true);
+xhr.send(null);
+var responseObject;
+var rColumnContent = '';
+
+xhr.onload = function(data) {
+	if (xhr.status === 200) {
+		// console.log(data);
+		responseObject = JSON.parse(xhr.responseText);	
+		// console.log(responseObject);
+		// console.log(responseObject[2]);
+
+		for (var i = 0; i < responseObject.length; i++) {
+			rColumnContent += responseObject[i].title
+		}
+
+	}
+}
+
+$('.leftColumn li').on('click', function() {
+	var num = this.dataset.attribute;
+	// $('.rightColumn p').html(rColumnContent);
+	$('.rightColumn p').html(responseObject[num].title);
+});
